7214e9f5f957d0e031cbf49ac9fbe25b5c2ad126
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-01 18:14:11 -0300

    NumerosImares

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Exercício do site URI "Numeros impares"
+// Neste exercício o usuário irá digitar um numero inteiro. O programa vai executar mostrando a sequnêcia de numeros impares até o número digitado.
+
+
+int X = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= X; i++)
+{
+    if (i % 2 != 0){
+    Console.WriteLine(i);
+    }
+}
+
+
+
